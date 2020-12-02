e753dbe980b40c5cad42d7956d557b6c750fa15c
Anthony Vanegas <avanegas04@gmail.com>
2020-12-02 00:38:52 -0500

    update try except

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ vidTitles = []
 # get list of all track ids to import to spotify
 all_track_ids = []
 
-""" YOUTUBE """
+######## YOUTUBE ########
 def fetch_youtube_video_titles(youtube_dev_key, youtube_playlist_id):
 
     YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -31,52 +31,37 @@ def fetch_youtube_video_titles(youtube_dev_key, youtube_playlist_id):
         YOUTUBE_API_VERSION, 
         developerKey=youtube_dev_key)
 
-    request = youtube.playlistItems().list(
-        part="snippet,contentDetails",
-        maxResults=25,
-        playlistId= youtube_playlist_id
-    ).execute()
-
-    # serializing json
-    json_object = json.dumps(request, indent = 4)
-
-    previousPlaylistCount = 0
-    futurePlaylistCount = 0
-
-    # if there is a data.json file already then compare with new 
-    val = path.exists("data.json")
-    if (val):
-        with open('data.json', 'r') as openFile:
-            data = json.load(openFile)
-            dataItems1 = data['items']
-        # count how many song title are in old data.json file 
-        previousPlaylistCount = len(dataItems1)    
-    
-    # write the new response to a json file
-    with open('data.json', 'w') as outfile:
-        outfile.write(json_object)
+    # try to grab youtube playlist data
+    try:
+        request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            maxResults=25,
+            playlistId= youtube_playlist_id
+        ).execute()
+    except:
+        print("Error getting Youtube playlist")
+        exit()
+    # if no exceptions continue running the following code
+    else:
+        # serializing json
+        json_object = json.dumps(request, indent = 4)
         
-    # read the new json file
-    with open('data.json', 'r') as openFile:
-        data2 = json.load(openFile)
-        dataItems2 = data2['items']
-        # count how many song title are in the new data.json file
-        futurePlaylistCount = len(dataItems2)
-    
-    # return true if the new playlist info pulled has a discrepency in # of tracks
-    if not previousPlaylistCount == futurePlaylistCount:
+        # write the new response to a json file
+        with open('data.json', 'w') as outfile:
+            outfile.write(json_object)
+
+        # read the new json file
+        with open('data.json', 'r') as openFile:
+            data2 = json.load(openFile)
+
+        # grab the titles of the videos in my playlist and append to array
         for a in data2['items']:
-            #grab the titles of the videos in my playlist
             vidTitles.append(a['snippet']['title'])
 
-        return 1
-    
-
-    # if there is no difference in track playlist # then return false
-    return 0
+        return True
 
 
-""" SPOTIFY """
+######## SPOTIFY ########
 def add_tracks_to_spotify(
     sp_client_id, 
     sp_client_secret, 
@@ -93,87 +78,92 @@ def add_tracks_to_spotify(
 
     scope = "playlist-modify-public, playlist-modify-private, user-read-recently-played"
     
-    # generate token 
-    token = util.prompt_for_user_token(
-        user,scope,
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirectURI
-        )
-    sp = spotipy.Spotify(auth=token)
+    try:
+        # generate token 
+        token = util.prompt_for_user_token(
+            user,scope,
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirectURI
+            )
+        sp = spotipy.Spotify(auth=token)
+    except:
+        print("Error spotify auth")
+        exit()  
+    else:
+        # ignore these captions if it comes in the name of the youtube song
+        ignoreCaption = [
+            '(official video)',
+            '[official video]',
+            '(video oficial)',
+            '( video oficial )',
+            '[video oficial]',
+            '(official music video)',
+            '( official music video )',
+            '[official music video]',
+            '(official audio)',
+            '[official audio]',
+            '(instrumental)',
+            '(Versión Urbana - Official Video)',
+            '(Animated Video)', 
+            '(music video)',
+            '[music video]',
+            '(audio / remix)',
+            '(full version)',
+            '(Official Lyric Video)',
+            '(Clip officiel)',
+            '(Dirty Version)',
+            'Official Music Video',
+            "|",
+            "(Audio)",
+            "(Official Performance Video)"
+        ]
 
+        for songs in vidTitles:
 
-    # ignore this captions if it comes in the name of the youtube song
-    ignoreCaption = [
-        '(official video)',
-        '[official video]',
-        '(video oficial)',
-        '( video oficial )',
-        '[video oficial]',
-        '(official music video)',
-        '( official music video )',
-        '[official music video]',
-        '(official audio)',
-        '[official audio]',
-        '(instrumental)',
-        '(Versión Urbana - Official Video)',
-        '(Animated Video)', 
-        '(music video)',
-        '[music video]',
-        '(audio / remix)',
-        '(full version)',
-        '(Official Lyric Video)',
-        '(Clip officiel)',
-        '(Dirty Version)',
-        'Official Music Video',
-        "|",
-        "(Audio)"
-    ]
+            for ig in ignoreCaption:
+                # convert both strings to lowercase for easier comparison
+                lowercaseCaption = ig.lower()
+                songName = songs.lower()
+                if lowercaseCaption in songName:
+                    # if song has one of the ignoreCaptions then replace it with empty string
+                    songs = songName.replace(lowercaseCaption,"")
 
-    for songs in vidTitles:
+            # search for x song titles
+            results = sp.search(q=songs, limit=1, type='track')
 
-        for ig in ignoreCaption:
-            # convert both strings to lowercase for easier comparison
-            lowercaseCaption = ig.lower()
-            songName = songs.lower()
-            if lowercaseCaption in songName:
-                # if song has one of the ignoreCaptions then replace it with empty string
-                songs = songName.replace(lowercaseCaption,"")
+            # grab track ID to be used to add to spotify playlist
+            for track in results['tracks']['items']:
+                # debug make sure I am grabbing the correct songs
+                print("Song name : {} --- uri : {}".format(track['name'], track['uri']))
+                trackID = track['id']
+                all_track_ids.append(trackID)    
+        
+        # grabs songs from current spotify plalist
+        playlist_tracks = sp.user_playlist_tracks(user=user, playlist_id=sp_playlist, fields='items,uri,name,id,total')
+        
+        """ 
+        If the track id already exists in spotify playlist 
+        then remove it so that duplicate are not created
+        """
+        for pt in playlist_tracks['items']:
+            # grab track id
+            pl_id = pt['track']['id']
 
-        # search for x song titles
-        results = sp.search(q=songs, limit=1, type='track')
+            # remove track id that already exist in the spotify playlist
+            if pl_id in all_track_ids:
+                all_track_ids.remove(pl_id)
 
-        # grab track ID to be used to add to spotify playlist
-        for track in results['tracks']['items']:
-            # debug make sure I am grabbing the correct songs
-            #print("name : {} - uri : {}".format(track['name'], track['uri']))
-            trackID = track['id']
-            all_track_ids.append(trackID)    
-    
-    # grabs songs from current spotify plalist
-    playlist_tracks = sp.user_playlist_tracks(user=user, playlist_id=sp_playlist, fields='items,uri,name,id,total')
-    
-    """ 
-    If the track id already exists in spotify playlist 
-    then remove it so that duplicate are not created
-    """
-    for pt in playlist_tracks['items']:
-        # grab track id
-        pl_id = pt['track']['id']
+        # check if there are tracks that need to be added to playlist   
+        if all_track_ids:
+            # add the tracks to my spotify playlist
+            sp.user_playlist_add_tracks(
+                user=user,
+                playlist_id=sp_playlist,
+                tracks=all_track_ids,
+                position=None
+            )
 
-        # remove track id that already exist in the spotify playlist
-        if pl_id in all_track_ids:
-            all_track_ids.remove(pl_id)
-
-    # check if there are tracks that need to be added to playlist   
-    if all_track_ids:
-        # add the tracks to my spotify playlist
-        sp.user_playlist_add_tracks(
-            user=user,
-            playlist_id=sp_playlist,
-            tracks=all_track_ids,
-            position=None
-        )
 
 if __name__ == "__main__":
     
